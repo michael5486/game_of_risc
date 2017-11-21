11ec681f1d22675d401aef068b8793fed7aa6e1c
michael5486 <michael5486@gmail.com>
2017-11-21 14:44:10 -0500

    basic form works

--- the_game_of_risc/decisions/urls.py
+++ the_game_of_risc/decisions/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<adjudication_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<adjudication_id>[0-9]+)/$', views.result, name='result'),
+    url(r'^new/$', views.decision_new, name='decision_new')
 
 ]
--- the_game_of_risc/decisions/views.py
+++ the_game_of_risc/decisions/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponseRedirect
+from django.utils import timezone
 
 from .models import Adjudication
 
@@ -17,21 +18,21 @@ def index(request):
 #     return render(request, 'decisions/detail.html', {'adjudication': adjudication})
 
 
-def detail(request, adjudication_id, form):
-    if request.method == "POST":
-        form = DecisionForm()
-        if form.is_valid():
-            newDecision = form.save(commit=False)
-            newDecision.save()
-            return render(request, 'decisions/result.html', {'form': form})
-    else:
-        form = DecisionForm()
+def detail(request, adjudication_id):
+    # if request.method == "POST":
+    #     form = DecisionForm()
+    #     if form.is_valid():
+    #         newDecision = form.save(commit=False)
+    #         newDecision.save()
+    #         return render(request, 'decisions/result.html', {'form': form})
+    # else:
+    #     form = DecisionForm()
     adjudication = get_object_or_404(Adjudication, pk=adjudication_id)
     return render(request, 'decisions/detail.html', {'adjudication': adjudication})
 
 def result(request):
     if request.method == "POST":
-        form = DecisionForm()
+        form = DecisionForm(request.POST)
         if form.is_valid():
             newDecision = form.save(commit=False)
             newDecision.save()
@@ -39,3 +40,19 @@ def result(request):
         else:
             form = DecisionForm()
     return render(request, 'decisions/result.html', {'form': form})
+
+def decision_new(request):
+    if request.method == "POST":
+        form = DecisionForm(request.POST)
+        if form.is_valid():
+            decision = form.save(commit=False)
+            decision.timestamp = timezone.now()
+            decision.save()
+            return redirect('https://www.google.com')
+    else:
+        form=DecisionForm()
+    return render(request, 'decisions/decision_edit.html', {'form': form})
+
+
+
+
