aaa121c7873ec256334794474eeb2695e831be88
michael5486 <michael5486@gmail.com>
2017-11-21 16:05:14 -0500

    got random case to work

--- the_game_of_risc/decisions/urls.py
+++ the_game_of_risc/decisions/urls.py
@@ -6,7 +6,6 @@ app_name = 'decisions'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<adjudication_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<adjudication_id>[0-9]+)/$', views.result, name='result'),
-    url(r'^new/$', views.decision_new, name='decision_new')
+    url(r'^random/$', views.random, name='random')
 
 ]
--- the_game_of_risc/decisions/views.py
+++ the_game_of_risc/decisions/views.py
@@ -13,10 +13,6 @@ def index(request):
     context = {'random_adjudication_list': random_adjudication_list}
     return render(request, 'decisions/index.html', context)
 
-# def detail(request, adjudication_id):
-#     adjudication = get_object_or_404(Adjudication, pk=adjudication_id)
-#     return render(request, 'decisions/detail.html', {'adjudication': adjudication})
-
 
 def detail(request, adjudication_id):
     correct_response = -1
@@ -36,28 +32,23 @@ def detail(request, adjudication_id):
 
     return render(request, 'decisions/detail.html', {'adjudication': adjudication, 'form': form, 'correct_response':correct_response})
 
-def result(request):
-    if request.method == "POST":
-        form = DecisionForm(request.POST)
-        if form.is_valid():
-            newDecision = form.save(commit=False)
-            newDecision.save()
-            return redirect('decisions/result.html', pk=adjudication_id)
-        else:
-            form = DecisionForm()
-    return render(request, 'decisions/result.html', {'form': form})
+def random(request):
+    random_adjudication = Adjudication.objects.order_by('?')[:2]
+    # adjudication = get_object_or_404(Adjudication, pk=adjudication_id)
+    return redirect('/decisions/{}'.format(random_adjudication[0].id))
 
-def decision_new(request):
-    if request.method == "POST":
-        form = DecisionForm(request.POST)
-        if form.is_valid():
-            decision = form.save(commit=False)
-            decision.timestamp = timezone.now()
-            decision.save()
-            return redirect('https://www.google.com')
-    else:
-        form=DecisionForm()
-    return render(request, 'decisions/decision_edit.html', {'form': form})
+
+# def decision_new(request):
+#     if request.method == "POST":
+#         form = DecisionForm(request.POST)
+#         if form.is_valid():
+#             decision = form.save(commit=False)
+#             decision.timestamp = timezone.now()
+#             decision.save()
+#             return redirect('https://www.google.com')
+#     else:
+#         form=DecisionForm()
+#     return render(request, 'decisions/decision_edit.html', {'form': form})
 
 
 
